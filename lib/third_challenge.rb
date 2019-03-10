8c753bb0250d49d715dc2120167a36597d3f0d3b
Donna Amos <donnacamos.developer@gmail.com>
2019-03-10 22:53:59 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -8,7 +8,7 @@ def third_challenge
        :name => {}, :age => {}
         },
       :hero => {
-        :name => {}, :age => {}
+        :name => {}, :age => {}, :status => {} 
         },
       :hero_friends => [:name => {}, :age => {}, :attitude => {}]
    }, 
